1726e22678cab7d71a566b11dab71eaaf1aaad89
Jason Marshall <jasonmarshall1994@gmail.com>
2024-03-09 12:45:17 -0500

    Added update and delete functionality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Traversy Media Typicode Todo Mini-Project using the fetch api and https://jsonplaceholder.typicode.com/ public test API
+Traversy Media Typicode Todo Mini-Project using the fetch api and https://jsonplaceholder.typicode.com/ public test API.
+
+This mini project focuses on using fetch API methods.
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiURL = 'https://jsonplaceholder.typicode.com/todos';
 
 const getTodos = () => {
-  fetch(apiURL + '?_limit=5').then((res) =>
+  fetch(apiURL + '?_limit=10').then((res) =>
     res.json().then((data) => {
       data.forEach((todo) => {
         addTodoToDOM(todo);
@@ -12,6 +12,7 @@ const getTodos = () => {
 
 const addTodoToDOM = (todo) => {
   const div = document.createElement('div');
+  div.classList.add('todo');
   div.appendChild(document.createTextNode(todo.title));
   div.setAttribute('data-id', todo.id);
 
@@ -41,9 +42,42 @@ const createTodo = (e) => {
     .then((data) => addTodoToDOM(data));
 };
 
+const toggleCompleted = (e) => {
+  if (e.target.classList.contains('todo')) {
+    e.target.classList.toggle('done');
+
+    updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
+  }
+};
+
+const updateTodo = (id, completed) => {
+  fetch(`${apiURL}/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ completed }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+const deleteTodo = (e) => {
+  if (e.target.classList.contains('todo')) {
+    const id = e.target.dataset.id;
+    fetch(`${apiURL}/${id}`, {
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then(() => e.target.remove());
+  }
+};
+
 const init = () => {
   document.addEventListener('DOMContentLoaded', getTodos);
   document.querySelector('#todo-form').addEventListener('submit', createTodo);
+  document
+    .querySelector('#todo-list')
+    .addEventListener('click', toggleCompleted);
+  document.querySelector('#todo-list').addEventListener('dblclick', deleteTodo);
 };
 
 init();
